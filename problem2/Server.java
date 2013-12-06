ae7cd65f55c381a34f3d8850bd3c754dc22648f9
Kevin O'Connor <kevin.oconnor7@gmail.com>
2013-12-06 14:55:43 -0500

    Comment problem 2 code

--- problem2/Server.java
+++ problem2/Server.java
@@ -1,3 +1,8 @@
+/*
+ * Prog. Lang. Project 3 Problem 2
+ * Kevin O'Connor & Dimitre Dimitrov
+**/
+
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
@@ -6,7 +11,6 @@ import java.util.Random;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
-import java.util.ArrayList;
 
 class constants {
 	public static final int A = 0;
@@ -199,12 +203,13 @@ class Worker implements Runnable{
 			this.acctId = acctNameToInt(name);
 		}
 
-
+		//Does this cache need to be verified?
 		public boolean needsVerify()
 		{
 			return (reader);
 		}
 
+		//Attempt to verify the initialValue of the cache
 		public void verify() throws TransactionAbortException
 		{
 			if(needsVerify() && locked)
@@ -214,12 +219,11 @@ class Worker implements Runnable{
 					acct.verify(initialValue);
 				} catch (TransactionAbortException up) {
 					throw up;
-				} catch (Exception e) {
-					System.out.println(e);
 				}
 			}
 		}
 
+		//Attempt to acquire the required locks for the cache
 		public void lock() throws TransactionAbortException
 		{
 			if(!reader && !writer) return;
@@ -236,6 +240,7 @@ class Worker implements Runnable{
 			locked = true;
 		}
 
+		//Release the acquired locks, if any exist
 		public void release() throws TransactionUsageError
 		{
 			if(!locked) return;
@@ -249,20 +254,24 @@ class Worker implements Runnable{
 			}
 		}
 
+		//Set the cache as writer
 		public void setWriter()
 		{
 			writer = true;
 			initialValue = peek();
 		}
 
+		//Set the cache as reader
 		public void setReader()
 		{
 			reader = true;
 			initialValue = peek();
 		}
 
+		//Unsafe read of the account's value
 		public int peek()
 		{
+			//We only want to peek once to make sure call used the same initalValue
 			if(!peeked)
 			{
 				initialValue = acct.peek();
@@ -272,6 +281,7 @@ class Worker implements Runnable{
 		}
 	}
 
+	//Given a token, return the account ID (raw, no derefenching)
 	private int acctNameToInt(String name) {
 		int accountNum = (int) (name.charAt(0)) - (int) 'A';
 		if (accountNum < A || accountNum > Z)
@@ -279,21 +289,22 @@ class Worker implements Runnable{
 		return accountNum;
 	}
 
-	// TO DO: parseAccount currently returns a reference to an account.
-	// You probably want to change it to return a reference to an
-	// account *cache* instead.
-	// used peeks for this
+	//Return a reference to an account given a token
 	private Account parseAccount(String name, Cache[] cacheaccts) {
 		int accountNum = parseAccountNum(name, cacheaccts);
 		return accounts[accountNum];
 	}
 
+	//Return the ID of an account given a token
 	private int parseAccountNum(String name, Cache[] cacheaccts) {
 		int accountNum = acctNameToInt(name);
-		Account a = accounts[accountNum];
+
+		//Try to derefence the account ID if needed
 		for (int i = 1; i < name.length(); i++) {
 			if (name.charAt(i) != '*')
 				throw new InvalidTransactionError();
+
+			//We need to verify this value later to make sure we defrenced the correct accounts
 			cacheaccts[accountNum].reader = true;
 			accountNum = (cacheaccts[accountNum].peek() % numLetters);
 		}
@@ -301,6 +312,7 @@ class Worker implements Runnable{
 	}
 
 	private int parseNum(String name) {
+		//Ensure it's a valid numeric string and return its integer value
 		if (name.charAt(0) >= '0' && name.charAt(0) <= '9') {
 			return new Integer(name).intValue();
 		}
@@ -309,27 +321,38 @@ class Worker implements Runnable{
 
 	private void releaseAccounts(Cache[] cacheaccts)
 	{
+		//Issue a release to every account in the cache, we silence any exceptions trying to release
+		//We don't really care if we fail to release, it means someone else initiated the lock
 		for (int j = 0; j < cacheaccts.length; j++) {
 			cacheaccts[j].release();
 		}
 	}
 
+	//Thread runner
 	public void run() {
+
 		// tokenize transaction
 		String[] commands = transaction.split(";");
 
+		//Parse all command independently
 		for (int i = 0; i < commands.length; i++)
 		{
+
+			//Build a cache of the accounts
 			Cache[] cacheaccts = new Cache[accounts.length];
 			for(int j = 0; j < accounts.length; ++j)
 			{
 				cacheaccts[j] = new Cache(accounts[j], String.valueOf((char)(j + (int)'A'))); 
 			}
 
+			//Split each token by whitespace
 			String[] words = commands[i].trim().split("\\s");
+
+
 			if (words.length < 3)
 				throw new InvalidTransactionError();
 			
+			//Retrieve the accounts on the LHS and mark it as needing a write lock
 			int lhsnum = parseAccountNum(words[0], cacheaccts);
 			Account lhs = accounts[lhsnum];
 			cacheaccts[lhsnum].setWriter();
@@ -337,30 +360,37 @@ class Worker implements Runnable{
 			if (!words[1].equals("="))
 				throw new InvalidTransactionError();
 
+			//Holder for RHS math
 			int val = 0;
 
-			// We can either have a letter (A-Z) or a number here
+			//Marker for the token we want to look at
 			int idx = 2;
 
+			//Is a number
 			if (words[idx].charAt(0) >= '0' && words[idx].charAt(0) <= '9')
 			{
 				val = parseNum(words[idx]);
 			}
+			//Is a valid account
 			else if (words[idx].charAt(0) >= 'A' && words[idx].charAt(0) <= 'Z')
 			{
+				//Pull the accounts number (derefference if needed), mark it as a reader, and peek
 				int acctnum = parseAccountNum(words[idx], cacheaccts);
 				cacheaccts[acctnum].setReader();
 				val = cacheaccts[acctnum].peek();
 			}
+			//Invalid token
 			else
 			{
 				throw new InvalidTransactionError();
 			}
 
+			// We can optionally either have a plus or minus and then a letter (A-Z) or number here
 			if(words.length == 5)
 			{
-				// We can either have a plus or minus and then a letter (A-Z) or number here
 				idx = 4;
+
+				//Is a number
 				if (words[idx].charAt(0) >= '0' && words[idx].charAt(0) <= '9')
 				{
 					if (words[idx-1].equals("+"))
@@ -370,8 +400,10 @@ class Worker implements Runnable{
 					else
 						throw new InvalidTransactionError();
 				} 
+				//Is a valid account
 				else if (words[idx].charAt(0) >= 'A' && words[idx].charAt(0) <= 'Z')
 				{
+					//Pull the accounts number (derefference if needed), mark it as a reader, and peek
 					int acctnum = parseAccountNum(words[idx], cacheaccts);
 					cacheaccts[acctnum].setReader();
 					if (words[idx-1].equals("+"))
@@ -387,6 +419,7 @@ class Worker implements Runnable{
 				}
 			}
 
+			//Continuously try to acquire the needed locks
 			boolean gotLocks = false;
 			while(!gotLocks)
 			{
@@ -395,12 +428,15 @@ class Worker implements Runnable{
 						cacheaccts[j].lock();
 					}
 				} catch (TransactionAbortException e) {
+					//If we fail to grab any lock, we want to release all of out locks
 					releaseAccounts(cacheaccts);
 					continue;
 				}
 				gotLocks = true;
 			}
 
+			//We want to try once to verify our values, if we fail we should repeat this entire
+			//process again to we have the updated information
 			try {
 				for (int j = 0; j < cacheaccts.length; j++) {
 					cacheaccts[j].verify();
@@ -411,7 +447,10 @@ class Worker implements Runnable{
 				continue;
 			}
 
+			//Got out locks and verified, it's now safe to update
 			lhs.update(val);
+
+			//Release our locks
 			releaseAccounts(cacheaccts);
 		}
 		System.out.println("commit: " + transaction);
